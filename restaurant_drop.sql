1d6f0e93ee86601e084d3979a2de69b609d00778
Szymon Golebiowski <szymon13gol@gmail.com>
2022-01-02 12:09:28 +0100

    fixed restaurant_drop

--- restaurant_drop.sql
+++ restaurant_drop.sql
@@ -1,223 +1,61 @@
 -- Created by Vertabelo (http://vertabelo.com)
 -- Last modification date: 2022-01-02 09:57:39.384
 
--- tables
--- Table: CompanyCustomers
-CREATE TABLE CompanyCustomers (
-    CustomerID int  NOT NULL,
-    CompanyName nvarchar(64)  NOT NULL,
-    NIP varchar(16)  NULL,
-    CONSTRAINT CompanyCustomers_pk PRIMARY KEY  (CustomerID)
-);
-
--- Table: Constants
-CREATE TABLE Constants (
-    Date datetime  NOT NULL,
-    Z1 int  NOT NULL,
-    K1 int  NOT NULL,
-    R1 int  NOT NULL,
-    K2 int  NOT NULL,
-    R2 int  NOT NULL,
-    D1 int  NOT NULL,
-    WZ int  NOT NULL,
-    WK int  NOT NULL,
-    CONSTRAINT ConstantChecks CHECK (Z1 >= 0 AND  K1 >= 0 AND  R1 >= 0 AND R1 <= 100 AND K2 >= 0 AND R2 >= 0 AND R2 <= 100 AND D1 >= 0 AND WZ >= 0 AND WK >= 0 ),
-    CONSTRAINT Constants_pk PRIMARY KEY  (Date)
-);
-
--- Table: Customers
-CREATE TABLE Customers (
-    CustomerID int  NOT NULL IDENTITY(1, 1),
-    Email nvarchar(64)  NULL,
-    Phone varchar(16)  NULL,
-    Address nvarchar(64)  NULL,
-    City nvarchar(64)  NULL,
-    PostalCode varchar(16)  NULL,
-    Country nvarchar(64)  NULL,
-    CONSTRAINT Customers_pk PRIMARY KEY  (CustomerID)
-);
-
--- Table: Invoices
-CREATE TABLE Invoices (
-    InvoiceID varchar(16)  NOT NULL,
-    Date datetime  NOT NULL,
-    TotalAmount money  NOT NULL,
-    FirstName nvarchar(64)  NULL,
-    LastName nvarchar(64)  NULL,
-    CompanyName nvarchar(64)  NULL,
-    Email nvarchar(64)  NULL,
-    Phone varchar(16)  NULL,
-    Address nvarchar(64)  NULL,
-    City nvarchar(64)  NULL,
-    PostalCode varchar(16)  NULL,
-    Country nvarchar(64)  NULL,
-    CONSTRAINT PositiveTotalAmount CHECK (TotalAmount > 0),
-    CONSTRAINT Invoices_pk PRIMARY KEY  (InvoiceID)
-);
-
--- Table: Meals
-CREATE TABLE Meals (
-    MealID int  NOT NULL IDENTITY(1, 1),
-    Name nvarchar(64)  NOT NULL,
-    SeaFood bit  NOT NULL,
-    DefaultPrice money  NOT NULL,
-    Active bit  NOT NULL,
-    CONSTRAINT PositiveDefaultPrice CHECK (DefaultPrice > 0),
-    CONSTRAINT Meals_pk PRIMARY KEY  (MealID)
-);
-
--- Table: Menu
-CREATE TABLE Menu (
-    MenuID int  NOT NULL IDENTITY(1, 1),
-    StartDate datetime  NOT NULL,
-    EndDate datetime  NOT NULL,
-    CONSTRAINT MenuStartBeforeEnd CHECK (StartDate < EndDate),
-    CONSTRAINT Menu_pk PRIMARY KEY  (MenuID)
-);
-
--- Table: MenuItems
-CREATE TABLE MenuItems (
-    MenuID int  NOT NULL,
-    MealID int  NOT NULL,
-    Price money  NOT NULL,
-    CONSTRAINT PositivePrice CHECK (Price > 0),
-    CONSTRAINT MenuItems_pk PRIMARY KEY  (MenuID,MealID)
-);
-
--- Table: OrderDetails
-CREATE TABLE OrderDetails (
-    OrderID int  NOT NULL,
-    Number int  NOT NULL,
-    MealID int  NOT NULL,
-    MenuID int  NOT NULL,
-    CONSTRAINT PositiveMenuNumber CHECK (Number > 0),
-    CONSTRAINT OrderDetails_pk PRIMARY KEY  (OrderID)
-);
-
--- Table: OrderDiscounts
-CREATE TABLE OrderDiscounts (
-    OrderID int  NOT NULL,
-    Discount decimal(5,2)  NOT NULL,
-    DiscountType int  NOT NULL,
-    CONSTRAINT DiscountRange CHECK (Discount >= 0 AND Discount <= 1),
-    CONSTRAINT OrderDiscounts_pk PRIMARY KEY  (OrderID,DiscountType)
-);
-
--- Table: Orders
-CREATE TABLE Orders (
-    OrderID int  NOT NULL IDENTITY(1, 1),
-    CustomerID int  NOT NULL,
-    ReservationID int  NULL,
-    OrderDate datetime  NOT NULL,
-    CompletionDate datetime  NULL,
-    Paid bit  NOT NULL,
-    InvoiceID varchar(16)  NULL,
-    CONSTRAINT OrderedBeforeCompleted CHECK (CompletionDate >= OrderDate),
-    CONSTRAINT Orders_pk PRIMARY KEY  (OrderID)
-);
-
--- Table: PrivateCustomers
-CREATE TABLE PrivateCustomers (
-    CustomerID int  NOT NULL,
-    FirstName nvarchar(64)  NOT NULL,
-    LastName nvarchar(64)  NOT NULL,
-    CONSTRAINT PrivateCustomers_pk PRIMARY KEY  (CustomerID)
-);
-
--- Table: Reservations
-CREATE TABLE Reservations (
-    ReservationID int  NOT NULL IDENTITY(1, 1),
-    StartDate datetime  NOT NULL,
-    EndDate datetime  NOT NULL,
-    Accepted bit  NOT NULL,
-    CustomerID int  NOT NULL,
-    Guests nvarchar(max)  NULL,
-    Canceled bit  NOT NULL,
-    CONSTRAINT ReservationStartBeforeEnd CHECK (StartDate < EndDate),
-    CONSTRAINT Reservations_pk PRIMARY KEY  (ReservationID)
-);
-
--- Table: TableDetails
-CREATE TABLE TableDetails (
-    TableID int  NOT NULL,
-    ReservationID int  NOT NULL,
-    CONSTRAINT TableDetails_pk PRIMARY KEY  (TableID,ReservationID)
-);
-
--- Table: Tables
-CREATE TABLE Tables (
-    TableID int  NOT NULL IDENTITY(1, 1),
-    Seats int  NOT NULL,
-    Active bit  NOT NULL,
-    CONSTRAINT PositiveSeats CHECK (Seats > 0),
-    CONSTRAINT Tables_pk PRIMARY KEY  (TableID)
-);
-
 -- foreign keys
--- Reference: Customers_CompanyCustomers (table: CompanyCustomers)
-ALTER TABLE CompanyCustomers ADD CONSTRAINT Customers_CompanyCustomers
-    FOREIGN KEY (CustomerID)
-    REFERENCES Customers (CustomerID);
+ALTER TABLE CompanyCustomers DROP CONSTRAINT Customers_CompanyCustomers;
 
--- Reference: Customers_PrivateCustomers (table: PrivateCustomers)
-ALTER TABLE PrivateCustomers ADD CONSTRAINT Customers_PrivateCustomers
-    FOREIGN KEY (CustomerID)
-    REFERENCES Customers (CustomerID);
+ALTER TABLE PrivateCustomers DROP CONSTRAINT Customers_PrivateCustomers;
 
--- Reference: Invoices_Orders (table: Orders)
-ALTER TABLE Orders ADD CONSTRAINT Invoices_Orders
-    FOREIGN KEY (InvoiceID)
-    REFERENCES Invoices (InvoiceID);
+ALTER TABLE Orders DROP CONSTRAINT Invoices_Orders;
 
--- Reference: MenuItems_Meals (table: MenuItems)
-ALTER TABLE MenuItems ADD CONSTRAINT MenuItems_Meals
-    FOREIGN KEY (MealID)
-    REFERENCES Meals (MealID);
+ALTER TABLE MenuItems DROP CONSTRAINT MenuItems_Meals;
 
--- Reference: MenuItems_OrderDetails (table: OrderDetails)
-ALTER TABLE OrderDetails ADD CONSTRAINT MenuItems_OrderDetails
-    FOREIGN KEY (MenuID,MealID)
-    REFERENCES MenuItems (MenuID,MealID);
+ALTER TABLE OrderDetails DROP CONSTRAINT MenuItems_OrderDetails;
 
--- Reference: Menu_MenuItems (table: MenuItems)
-ALTER TABLE MenuItems ADD CONSTRAINT Menu_MenuItems
-    FOREIGN KEY (MenuID)
-    REFERENCES Menu (MenuID);
+ALTER TABLE MenuItems DROP CONSTRAINT Menu_MenuItems;
 
--- Reference: Order_Reservations (table: Orders)
-ALTER TABLE Orders ADD CONSTRAINT Order_Reservations
-    FOREIGN KEY (ReservationID)
-    REFERENCES Reservations (ReservationID);
+ALTER TABLE Orders DROP CONSTRAINT Order_Reservations;
 
--- Reference: OrdersDiscounts_Orders (table: OrderDiscounts)
-ALTER TABLE OrderDiscounts ADD CONSTRAINT OrdersDiscounts_Orders
-    FOREIGN KEY (OrderID)
-    REFERENCES Orders (OrderID);
+ALTER TABLE OrderDiscounts DROP CONSTRAINT OrdersDiscounts_Orders;
 
--- Reference: Orders_Customers (table: Orders)
-ALTER TABLE Orders ADD CONSTRAINT Orders_Customers
-    FOREIGN KEY (CustomerID)
-    REFERENCES Customers (CustomerID);
+ALTER TABLE Orders DROP CONSTRAINT Orders_Customers;
 
--- Reference: Orders_OrderDetails (table: OrderDetails)
-ALTER TABLE OrderDetails ADD CONSTRAINT Orders_OrderDetails
-    FOREIGN KEY (OrderID)
-    REFERENCES Orders (OrderID);
+ALTER TABLE OrderDetails DROP CONSTRAINT Orders_OrderDetails;
 
--- Reference: Reservations_Customers (table: Reservations)
-ALTER TABLE Reservations ADD CONSTRAINT Reservations_Customers
-    FOREIGN KEY (CustomerID)
-    REFERENCES Customers (CustomerID);
+ALTER TABLE Reservations DROP CONSTRAINT Reservations_Customers;
 
--- Reference: Reservations_TableDetails (table: TableDetails)
-ALTER TABLE TableDetails ADD CONSTRAINT Reservations_TableDetails
-    FOREIGN KEY (ReservationID)
-    REFERENCES Reservations (ReservationID);
+ALTER TABLE TableDetails DROP CONSTRAINT Reservations_TableDetails;
 
--- Reference: TableDetails_Tables (table: TableDetails)
-ALTER TABLE TableDetails ADD CONSTRAINT TableDetails_Tables
-    FOREIGN KEY (TableID)
-    REFERENCES Tables (TableID);
+ALTER TABLE TableDetails DROP CONSTRAINT TableDetails_Tables;
+
+-- tables
+DROP TABLE CompanyCustomers;
+
+DROP TABLE Constants;
+
+DROP TABLE Customers;
+
+DROP TABLE Invoices;
+
+DROP TABLE Meals;
+
+DROP TABLE Menu;
+
+DROP TABLE MenuItems;
+
+DROP TABLE OrderDetails;
+
+DROP TABLE OrderDiscounts;
+
+DROP TABLE Orders;
+
+DROP TABLE PrivateCustomers;
+
+DROP TABLE Reservations;
+
+DROP TABLE TableDetails;
+
+DROP TABLE Tables;
 
 -- End of file.
 
