94f40dfc0bebdb9d492ce15a9a28158b0b74fbf3
domkvv <niczkaa.bo@gmail.com>
2022-01-02 12:48:03 +0100

    views

---
+++ views.sql
@@ -0,0 +1,17 @@
+create view CurrentOrders
+as select * from Orders
+where OrderDate >= getdate() and CompletionDate < getdate()
+
+create view OrderHist
+as select * from Orders where CompletionDate <= getdate()
+
+create view ReservationsToAccept
+as select * from Reservations where Accepted = 0
+
+create view SeafoodOrders
+as select *
+    from Orders O
+    inner join OrderDetails OD on O.OrderID = OD.OrderID
+    inner join MenuItems MI on OD.MenuID = MI.MenuID and OD.MealID = MI.MealID
+    inner join Meals M on M.MealID = MI.MealID
+where SeaFood = 1
