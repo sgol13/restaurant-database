c857b3ec32bfb99627bc588fc1d19ebf16e58a4e
domkvv <niczkaa.bo@gmail.com>
2022-01-07 21:30:53 +0100

    views update

--- views.sql
+++ views.sql
@@ -1,3 +1,6 @@
+DROP VIEW CurrentOrders
+GO
+
 --> Widoki
 --# CurrentOrders
 --- Pokazuje zamówienia w trakcie realizacji.
@@ -7,6 +10,9 @@ WHERE OrderDate <= GETDATE() AND GETDATE() < CompletionDate
 GO
 --<
 
+DROP VIEW OrderHist
+GO
+
 --> Widoki
 --# OrderHist
 --- Pokazuje historię zamówień.
@@ -15,6 +21,9 @@ AS SELECT OrderID, CustomerID, ReservationID, Paid, InvoiceID FROM Orders WHERE 
 GO
 --<
 
+DROP VIEW ReservationsToAccept
+GO
+
 --> Widoki
 --# ReservationsToAccept
 --- Pokazuje rezerwacje, które nie zostały zaakceptowane.
@@ -22,6 +31,9 @@ CREATE VIEW ReservationsToAccept
 AS SELECT ReservationID, CustomerID, Guests, Canceled FROM Reservations WHERE Accepted = 0
 GO
 --<
+
+DROP VIEW SeafoodOrders
+GO
 
 --> Widoki
 --# SeafoodOrders
@@ -36,6 +48,8 @@ WHERE SeaFood = 1
 GO
 --<
 
+DROP VIEW CurrentConstants
+GO
 
 --> Widoki
 --# CurrentConstants
@@ -45,4 +59,35 @@ AS SELECT TOP 1 c.Z1, c.K1, c.R1, c.K2, c.R2, c.D1, c.WZ, c.WK
     FROM Constants c
     ORDER BY c.[Date] DESC
 GO
---< 
+--<
+
+DROP VIEW CurrentMenu
+GO
+
+--> Widoki
+--# CurrentMenu
+--- Zwraca aktualne menu dla zamówień na ten sam dzień
+CREATE VIEW CurrentMenu
+AS SELECT MI.MenuID, MI.MealID, MI.Price
+FROM MenuItems MI INNER JOIN Menu M ON M.MenuID = MI.MenuID
+WHERE StartDate <= GETDATE() AND GETDATE() <= EndDate
+GO
+--<
+
+DROP VIEW TablesAvailableNow
+GO
+
+--> Widoki
+--# TablesAvailableNow
+--- Zwraca aktualnie dostępne stoliki
+CREATE VIEW TablesAvailableNow
+AS SELECT T.TableID, Seats
+FROM Tables T INNER JOIN TableDetails TD ON T.TableID = TD.TableID
+INNER JOIN Reservations R ON TD.ReservationID = R.ReservationID
+WHERE Active = 1 AND
+(T.TableID NOT IN (SELECT TableID FROM TableDetails)
+OR T.TableID NOT IN (SELECT TableID FROM TableDetails TD1
+    INNER JOIN Reservations R1 ON TD1.ReservationID = R1.ReservationID
+    WHERE R1.StartDate <= GETDATE() AND GETDATE() <= EndDate))
+GO
+--<
