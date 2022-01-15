84386f10b4ee588a31c103054d5b94a834f8e43f
Michał Gniadek <mgniadek@interia.pl>
2022-01-15 15:29:34 +0100

    meals procedures

---
+++ src/meals/meals_procedures.sql
@@ -0,0 +1,36 @@
+--> Procedury
+--# NewMeal(Name, IsSeaFood, DefaultPrice, Active = 1, MealID OUTPUT)
+--- Tworzy nowy posiłek.
+CREATE OR ALTER PROCEDURE NewMeal(@Name nvarchar(64), @IsSeaFood bit, @DefaultPrice money, @Active bit = 1, @MealID int OUTPUT)
+AS BEGIN
+    INSERT INTO Meals([Name], SeaFood, DefaultPrice, Active)
+    VALUES(@Name, @IsSeaFood, @DefaultPrice, @Active)
+    
+    SET @MealID = @@IDENTITY
+END
+GO
+--<
+
+--> Procedury
+--# SetMealActive(MealID, Active)
+--- Aktywuje lub dezaktywuje posiłek.
+CREATE OR ALTER PROCEDURE SetMealActive(@MealID int, @Active bit)
+AS BEGIN
+    UPDATE Meals
+    SET Active = @Active
+    WHERE MealID = @MealID
+END
+GO
+--<
+
+--> Procedury
+--# UpdateMealDefaultPrice(MealID, DefaultPrice)
+--- Zmienia podstawową cenę posiłku.
+CREATE OR ALTER PROCEDURE UpdateMealDefaultPrice(@MealID int, @DefaultPrice money)
+AS BEGIN
+    UPDATE Meals
+    SET DefaultPrice = @DefaultPrice
+    WHERE MealID = @MealID
+END
+GO
+--<
